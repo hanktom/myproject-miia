3392fb90581a3ffb2aa2eeaa94be2157e3504486
Hank Tom <hanktom@gmail.com>
2021-11-09 15:07:46 +0800

    stars line

---
+++ src/main/java/com/tom/StarArrow.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class StarArrow {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<11; i++) {
+
+            for (int j = 0; j <= 20; j++) {
+
+
+                    System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/com/tom/StarLine.java
+++ src/main/java/com/tom/StarLine.java
@@ -1,17 +1,16 @@
 package com.tom;
 
-public class Stars {
+public class StarLine {
     public static void main(String[] args) {
         for (int i = 0; i<10; i++) {
             for (int j = 0; j < 20; j++) {
-                if (i==8) {
-                    System.out.print(" ");
-                } else {
+                if (2*i==19-j) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
-
     }
 }
