af4766e15edd633e9f3d11459ff982908dfe676b
Hank Tom <hanktom@gmail.com>
2021-11-23 15:22:25 +0800

    Vending machine p4p5 choosing drinks

--- src/main/java/com/tom/Vending.java
+++ src/main/java/com/tom/Vending.java
@@ -4,30 +4,41 @@ import java.util.Scanner;
 
 public class Vending {
     public static void main(String[] args) {
+        int aPrice = 15;
+        int bPrice = 25;
         Scanner scanner = new Scanner(System.in);
         int total = 0;
         boolean end = false;
         while(!end) {
             System.out.println("Please put your coin(1/5/10):  Total:" + total);
             String s = scanner.next();
-            int num = Integer.parseInt(s);
-            switch (num) {
-                case 1:
-                    System.out.println("1");
-                    total = total + num;
-                    break;
-                case 5:
-                    total = total + num;
-                    break;
-                case 10:
-                    total = total + num;
-                    break;
-                case 0:
-                    end = true;
-                    break;
-                default:
-                    break;
+            try {
+                int num = Integer.parseInt(s);
+                switch (num) {
+                    case 1:
+                    case 5:
+                    case 10:
+                        total = total + num;
+                        break;
+                    case 0:
+                        end = true;
+                        break;
+                    default:
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("ah");
+                if (s.equals("a")) {
+                    System.out.println("A drink");
+                    if (total >= aPrice) {
+                        total = total - aPrice;
+                        System.out.println("DON!");
+                    } else {
+                        System.out.println("BEEP!");
+                    }
+                }
             }
+
         }
         System.out.println("END");
     }
